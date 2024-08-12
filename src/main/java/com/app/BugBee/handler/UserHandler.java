7016411dd924865a8ed35dda8105c2b2ef83ea0a
Rickin Shah <rickin.shah17403@gmail.com>
2024-08-12 13:12:12 +0530

    remove the repository abstraction and wrote the logic in handler

--- src/main/java/com/app/BugBee/handler/UserHandler.java
+++ src/main/java/com/app/BugBee/handler/UserHandler.java
@@ -3,12 +3,15 @@ package com.app.BugBee.handler;
 import com.app.BugBee.dto.BooleanAndMessage;
 import com.app.BugBee.dto.UserDto;
 import com.app.BugBee.dto.UserRegistrationDto;
-import com.app.BugBee.service.UserService;
+import com.app.BugBee.entity.User;
+import com.app.BugBee.repository.UserRepository;
+import com.app.BugBee.utils.AppUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.rsocket.RSocketProperties;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -16,40 +19,59 @@ import java.util.UUID;
 @Service
 public class UserHandler {
     @Autowired
-    private UserService service;
+    private UserRepository repository;
 
     public Mono<ServerResponse> getUsers(ServerRequest request) {
-        return ServerResponse.ok().body(service.getUsers(), UserDto.class);
+        return ServerResponse.ok().body(BodyInserters.fromPublisher(
+                repository.findAll()
+                        .map(AppUtils::UserToDto), UserDto.class
+        ));
     }
 
     public Mono<ServerResponse> getUser(ServerRequest request) {
-        Mono<String> email = request.bodyToMono(String.class);
-        return email.flatMap(e -> ServerResponse.ok().body(service.getUser(e), UserDto.class)).log();
+        return request.bodyToMono(String.class)
+                .flatMap(repository::findByEmail)
+                .map(AppUtils::UserToDto)
+                .flatMap(user -> ServerResponse.ok().body(BodyInserters.fromValue(user)));
     }
 
     public Mono<ServerResponse> saveUser(ServerRequest request) {
-        Mono<UserRegistrationDto> userRegistrationDtoMono = request.bodyToMono(UserRegistrationDto.class);
-        return service.saveUser(userRegistrationDtoMono)
-                .flatMap(p -> p.isBool()?
-                        ServerResponse.ok().body(Mono.just(p), BooleanAndMessage.class) :
-                        ServerResponse.badRequest().body(Mono.just(p), BooleanAndMessage.class));
+        return request.bodyToMono(UserRegistrationDto.class)
+                .filterWhen(user -> repository.existsByEmail(user.getEmail()).map(exists -> !exists))
+                .map(AppUtils::UserRegistrationToEntity)
+                .flatMap(repository::save)
+                .map(AppUtils::UserToDto)
+                .flatMap(user -> ServerResponse.ok().body(BodyInserters
+                                .fromValue(new BooleanAndMessage(true, "Registered Successfully!"))))
+                .switchIfEmpty(ServerResponse.badRequest().body(BodyInserters
+                        .fromValue(new BooleanAndMessage(false, "Email already exists!"))));
     }
 
     public Mono<ServerResponse> updatePassword(ServerRequest request) {
-        Mono<UserRegistrationDto> userRegistrationDtoMono = request.bodyToMono(UserRegistrationDto.class);
-        return service.updatePassword(userRegistrationDtoMono)
-                .flatMap(p-> p.isBool()?
-                        ServerResponse.ok().body(Mono.just(p), BooleanAndMessage.class) :
-                        ServerResponse.badRequest().body(Mono.just(p), BooleanAndMessage.class));
+        return request.bodyToMono(UserRegistrationDto.class)
+                .flatMap(user -> repository.findByEmail(user.getEmail())
+                        .doOnNext(e -> e.setPassword(user.getPassword())))
+                .flatMap(repository::save)
+                .flatMap(e -> ServerResponse.ok()
+                        .body(BodyInserters
+                                .fromValue(new BooleanAndMessage(true, "Password updated!"))))
+                .switchIfEmpty(ServerResponse.badRequest()
+                        .body(BodyInserters
+                                .fromValue(new BooleanAndMessage(false, "Invalid Email!"))));
+
     }
 
     public Mono<ServerResponse> deleteUser(ServerRequest request) {
-        UUID uuid = UUID.fromString(request.headers().header("token").getFirst());
-        return service.deleteUser(uuid).flatMap(e -> ServerResponse.noContent().build());
+        UUID uuid = UUID.fromString(request.headers().header("Authorization").getFirst());
+        return repository.deleteById(uuid)
+                .flatMap(e -> ServerResponse.noContent().build());
     }
 
     public Mono<ServerResponse> saveUsers(ServerRequest request) {
-        return ServerResponse.ok().body(service.saveUsers(), UserDto.class);
+        return ServerResponse.ok().body(Flux.range(1, 5000)
+                .map(i -> new User(null, "user " + i, "user " + i, "user " + i, "USER"))
+                .flatMap(repository::save)
+                .map(AppUtils::UserToDto), UserDto.class);
     }
 
 }
