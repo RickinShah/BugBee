68b680750da3eb629c77d8def588aaee3850d86b
Rickin Shah <rickin.shah17403@gmail.com>
2024-08-22 13:02:24 +0530

    Improved and refactor otp handler

--- src/main/java/com/app/BugBee/handler/OtpHandler.java
+++ src/main/java/com/app/BugBee/handler/OtpHandler.java
@@ -7,12 +7,9 @@ import com.app.BugBee.entity.User;
 import com.app.BugBee.repository.OtpRepository;
 import com.app.BugBee.repository.UserRepository;
 import com.app.BugBee.security.JwtTokenProvider;
-import jakarta.mail.internet.MimeMessage;
+import com.app.BugBee.utils.MailSenderUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -26,9 +23,6 @@ import java.security.SecureRandom;
 public class OtpHandler {
 
     @Autowired
-    private JavaMailSender mailSender;
-
-    @Autowired
     private UserRepository repository;
 
     @Autowired
@@ -37,50 +31,15 @@ public class OtpHandler {
     @Autowired
     private JwtTokenProvider tokenProvider;
 
-    @Value("${spring_email}")
-    private String from;
+    @Autowired
+    MailSenderUtils mailSender;
 
     public Mono<ServerResponse> sendOtp(ServerRequest request) {
         return request.bodyToMono(User.class)
                 .flatMap(user -> repository.findByEmail(user.getEmail()))
-                .flatMap(user -> {
-                    int otpValue = new SecureRandom().nextInt(100000, 1000000);
-                    return Mono.just(Otp.builder()
-                                    .otp(otpValue)
-                                    .userId(user.getId())
-                                    .expirationTime(System.currentTimeMillis() + 15 * 60 * 1000)
-                                    .build()
-                            )
-                            .flatMap(otpRepository::save)
-                            .map(otpMono -> {
-                                try {
-                                    MimeMessage mimeMessage = mailSender.createMimeMessage();
-                                    MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-                                    mimeMessageHelper.setFrom(from);
-                                    mimeMessageHelper.setTo(user.getEmail());
-                                    mimeMessageHelper.setSubject("One-Time Password from BugBee");
-                                    mimeMessageHelper.setText(
-                                            "Hello " + user.getName() + ", <strong>" + otpValue
-                                                    + "</strong> is your One-Time Password(OTP) from BugBee. OTP is valid upto next 15 minutes.",
-                                            true
-                                    );
-//                                    if(email.getAttachment() != null) {
-//                                        FileSystemResource file = new FileSystemResource(new File(attachment));
-//                                        mimeMessageHelper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
-//                                    }
-
-                                    mailSender.send(mimeMessage);
-                                    return new BooleanAndMessage(true, "Mail sent Successfully!");
-                                } catch (Exception e) {
-                                    log.info(e.toString());
-                                    return new BooleanAndMessage(false, "Unable to send Otp!");
-                                }
-
-                            })
-                            .defaultIfEmpty(new BooleanAndMessage(false, "Something went wrong!"));
-                })
+                .flatMap(this::createOtpAndSendOtpMail)
                 .flatMap(result -> ServerResponse.ok().body(BodyInserters.fromValue(
-                        new BooleanAndMessage(true, "OTP sent successfully!")))
+                        new BooleanAndMessage(result.isSuccess(), result.getMessage())))
                 )
                 .switchIfEmpty(ServerResponse.badRequest().body(BodyInserters.fromValue(
                         new BooleanAndMessage(false, "Invalid Email!")))
@@ -92,6 +51,7 @@ public class OtpHandler {
         return authOtpMono
                 .flatMap(authOtp -> repository.findByEmail(authOtp.getEmail())
                         .flatMap(user -> otpRepository.findById(user.getId()))
+                        .switchIfEmpty(Mono.error(new RuntimeException("OTP not found!")))
                         .filter(otp -> otp.getExpirationTime() >= System.currentTimeMillis())
                         .switchIfEmpty(Mono.error(new RuntimeException("OTP Expired!")))
                         .filter(otp -> otp.getOtp() == authOtp.getOtp())
@@ -107,4 +67,24 @@ public class OtpHandler {
                 );
 
     }
+
+    public Mono<BooleanAndMessage> createOtpAndSendOtpMail(User user) {
+        int otpValue = new SecureRandom().nextInt(100000, 1000000);
+        Otp otp = Otp.builder()
+                .otp(otpValue)
+                .userId(user.getId())
+                .expirationTime(System.currentTimeMillis() + 15 * 60 * 1000)
+                .build();
+
+        return otpRepository.save(otp)
+                .flatMap(otpObj -> repository.findById(otp.getUserId()))
+                .map(e -> mailSender.sendMail(user.getEmail(),
+                        "One-Time Password from BugBee",
+                        "Hello " + user.getName() + ", <strong>" + otpValue
+                                + "</strong> is your One-Time Password(OTP) from BugBee. OTP is valid upto next 15 minutes.",
+                        null
+                        ))
+                .defaultIfEmpty(new BooleanAndMessage(false, "Something went wrong!"));
+    }
+
 }
---
+++ src/main/java/com/app/BugBee/utils/MailSenderUtils.java
@@ -0,0 +1,48 @@
+package com.app.BugBee.utils;
+
+import com.app.BugBee.dto.BooleanAndMessage;
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.Objects;
+
+@Slf4j
+@Component
+public class MailSenderUtils {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${SPRING_EMAIL}")
+    private String from;
+
+    public BooleanAndMessage sendMail(String to, String subject, String body, String attachment) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setFrom(from);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(body, true);
+            if(attachment != null) {
+                FileSystemResource file = new FileSystemResource(new File(attachment));
+                mimeMessageHelper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
+            }
+
+            mailSender.send(mimeMessage);
+            return new BooleanAndMessage(true, "Mail sent Successfully!");
+
+        } catch (Exception e) {
+            log.info("Unable to send mail to {} due to {}", to, e.getMessage());
+            return new BooleanAndMessage(false, "Unable to send Mail!");
+        }
+
+    }
+}
